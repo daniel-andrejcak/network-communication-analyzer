13729b8800b6c23a0dd9204e3e1cade4f9ef0433
Daniel Andrejčák <89748946+daniel-andrejcak@users.noreply.github.com>
2023-09-23 00:39:22 +0200

    pomocny program na nacitanie protokolov z protocols.yaml

---
+++ protocols.py
@@ -0,0 +1,15 @@
+"""protocols.py sluzi na nacitanie protokolov z externeho suboru
+je to v osobitnom subore, aby sa zabranilo import loopingu
+a aby sa to nenachadzalo v subore frame.py, kvoli prehliadnosti"""
+
+from ruamel.yaml import YAML
+
+PROTOCOLSFILE = "protocols.yaml"
+
+yaml = YAML()
+
+protocolsFile = open(PROTOCOLSFILE, "r")
+
+protocols = dict(yaml.load(protocolsFile))
+
+protocolsFile.close()
